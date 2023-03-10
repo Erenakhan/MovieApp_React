1f7d7dfd2fe872c6120d90e7259af2d952decfeb
Erenakhan <mustafaakhan794@gmail.com>
2023-03-10 15:19:54 +0300

    header added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,37 +4,56 @@ import MovieChild from './Components/MovieChild';
 import './App.css'
 
 function App() {
+    const [className,setClassName] =useState("bottomButtons")
+    const handleMouseLeave =()=>{
+      setClassName("bottomButtons")
+    }
+    const handleMouseEnter =()=>{
+      setClassName("bottomButtonsE")
+    }
     const api_key =" a2c8c59db02aa468f8e8abcb8b5b8a7b"
-
+    const head =()=>{
+      setPage(1)
+    }
     const [page,setPage] =useState(1)
-    
     const nextPage =(()=>{
       setPage(page+1)
     })
     const prevPage =(()=>{
      page===1 ? setPage(1) : setPage(page-1)
     })
-
- 
-    console.log(page)
     const [movies,setMovies] = useState();
+
+    const [searchItems,setSearchItems] = useState();
+
     useEffect(()=>{
         fetch(`https://api.themoviedb.org/3/movie/popular?api_key=a2c8c59db02aa468f8e8abcb8b5b8a7b&page=${page}&include_adult=false&include`)
         .then(Response=>Response.json())
         .then(Response=>setMovies(Response.results
             ))
       },[page])
-      console.log(movies)
   return (
     <div className='movie-conteiner'>
-     
+      <header>
+        <div onClick={head} className='header-div'>
+        <h1 >MovieDB</h1>
+        <img className='icon' src="https://cdn-icons-png.flaticon.com/512/1179/1179120.png" />
+        </div>
+          <input type="text" onChange={((e)=>setSearchItems(e.target.value))} />
+      </header>
+
+      <hr></hr>
           <div className='grid'>
             {movies.map((e,i)=>
               <MovieChild key={i} {...e} />)}
           </div>
           <div className='buttons'>
-           <button className='bottomButtons' onClick={prevPage}>Prev</button>
-           <button className='bottomButtons' onClick={nextPage}>Next</button>
+           <button className= { className}
+            onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
+           onClick={prevPage}>Prev</button>
+           <button className={ className} 
+            onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
+           onClick={nextPage}>Next</button>
           </div>
     </div>
   )
--- my-app/src/Components/MovieChild.jsx
+++ my-app/src/Components/MovieChild.jsx
@@ -3,17 +3,23 @@ import '../App.css'
 import React, { useState } from 'react'
 
 function MovieChild({title,poster_path,vote_average,overview,release_date}) {
-    const img ="https://image.tmdb.org/t/p/w500"
+    const [className, setClassName] = useState("button")
+   const img ="https://image.tmdb.org/t/p/w500"
     const [show,setShow] =useState(false)
     const handleShow=()=>setShow(true)
     const handleClose=()=>setShow(false)
-  
+    const A =()=>{
+      setClassName("A")
+    }
+    const B =()=>{
+      setClassName("button")
+    }
   return (
     <>
   <div className="card ">
         <div className="card-body">
           <img className="card-img-top" src={img+poster_path} />
-         <button type='button'onClick={handleShow} className="button">
+         <button onMouseEnter={A} onMouseLeave={B} type='button'onClick={handleShow} className={className}>
             View more
           </button> 
           <div className="card-body">
@@ -35,7 +41,7 @@ function MovieChild({title,poster_path,vote_average,overview,release_date}) {
                 <h4>Imdb:{vote_average}</h4>
                 <h4>Release Date: {release_date}</h4>
                 <h4>Overview</h4>
-                <p>{overview}</p>
+                <p >{overview}</p>
             </Modal.Body>
 
             </Modal>
